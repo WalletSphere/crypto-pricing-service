330598f41403934a722bb3f30e88932f4c688256
Max <maksimhomisak64@gmail.com>
2024-01-03 19:56:52 +0100

    replaced !! with default value in null case
    replaced !! with default value in null case
    declare const of user-currencies url

--- src/main/kotlin/com/khomishchak/cryptopricingservice/service/ws/WebSocketServiceImpl.kt
+++ src/main/kotlin/com/khomishchak/cryptopricingservice/service/ws/WebSocketServiceImpl.kt
@@ -1,16 +1,17 @@
 package com.khomishchak.cryptopricingservice.service.ws
 
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.khomishchak.cryptopricingservice.model.integration.CryptoExchanger
 import com.khomishchak.cryptopricingservice.model.internall.exchanger.UsedToken
 import com.khomishchak.cryptopricingservice.model.internall.exchanger.UsedTokens
 import com.khomishchak.cryptopricingservice.service.integration.IntegrationWebSocketService
+import com.khomishchak.cryptopricingservice.utility.mapJsonResp
 import okhttp3.OkHttpClient
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
 
+const val GET_USED_CURRENCIES_URL = "http://localhost:8080/exchangers/used-currencies"
 @Service
 class WebSocketServiceImpl (private val integrationWebSocketConnectors: List<IntegrationWebSocketService>)
     : WebSocketService {
@@ -30,12 +31,11 @@ class WebSocketServiceImpl (private val integrationWebSocketConnectors: List<Int
             websocketIntegrations[exchanger]?.subscribe(accountId, tickers) ?: Unit
 
     override fun getLastPrices(accountId: Long, exchanger: CryptoExchanger, tickers: List<String>) =
-            websocketIntegrations[exchanger]!!.getLastPrices(accountId, tickers)
+            websocketIntegrations[exchanger]?.getLastPrices(accountId, tickers) ?: emptyMap();
 
     private fun subscribeToAlreadyFollowedTickers() {
-        val response = restTemplate.getForObject<String>("http://localhost:8080/exchangers/used-currencies")
-        val type = object : TypeToken<List<UsedToken>>() {}.type
-        val data = Gson().fromJson<List<UsedToken>>(response, type)
+        val response = restTemplate.getForObject<String>(GET_USED_CURRENCIES_URL)
+        val data = Gson().mapJsonResp<List<UsedToken>>(response)
 
         UsedTokens(data).records.forEach {
             websocketIntegrations[CryptoExchanger.valueOf(it.code)]
